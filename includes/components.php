982e77692e32f177a0a3ce89b589c6a05b27dd7a
MatrixMe <safosah00@gmail.com>
2025-04-08 15:08:56 +0000

    fix table overflow on dashboard

--- includes/components.php
+++ includes/components.php
@@ -302,7 +302,7 @@
         return "
             <div class=\"w-full overflow-hidden rounded-lg shadow-xs\">
               <div class=\"w-full overflow-x-auto\">
-                <table class=\"w-full whitespace-no-wrap $class_\" $attr>
+                <table class=\"w-full $class_\" $attr>
         ";
     }
     
