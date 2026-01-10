912b300058179af3c6047f9c33a2b0fe3bce7060
MatrixMe <67325727+MatrixMe00@users.noreply.github.com>
2026-01-10 20:18:54 +0000

    show username on profile dropdown

--- layouts/auth.php
+++ layouts/auth.php
@@ -176,6 +176,10 @@
                     aria-label="submenu"
                   >
                     <li class="flex">
+                      <span class="inline-flex items-center w-full px-2 py-1 text-sm"><?= user()["index_number"] ?? user()["username"] ?></span>
+                    </li>
+                    <hr>
+                    <li class="flex">
                       <a
                         class="inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
                         href="<?= url(
